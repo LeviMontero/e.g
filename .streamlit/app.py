616931284f7232c8c870bdc175c2b3433b23cd77
LeviMontero <levidelosmonteros@gmail.com>
2025-03-18 17:51:38 -0600

    Update app.py

--- .streamlit/app.py
+++ .streamlit/app.py
@@ -60,3 +60,31 @@ if start_button:
 
 # Mostrar el DataFrame con los resultados de todos los experimentos
 st.write(st.session_state['df_experiment_results'])
+
+# Este es el gráfico de líneas que irá actualizándose
+chart = st.line_chart([0.5])
+
+def toss_coin(n):
+    # Simula el lanzamiento de la moneda
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no += 1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])  # Actualiza el gráfico de líneas con la nueva media
+        time.sleep(0.05)
+
+    return mean
+
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    mean = toss_coin(number_of_trials)
